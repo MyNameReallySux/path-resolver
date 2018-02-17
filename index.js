8ba9892787849ddd60586c4ee4c81455a4a9a14f
Chris Coppola <mynamereallysux@gmail.com>
2018-02-17 14:19:33 -0500

    Refactoring Main Function

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ let { isObject } = require('@beautiful-code/type-utils')
 
 let { print, debug, error } = require('./dist/lib/ConsoleUtils')
 
-let { PathManager } = require('./dist/main')
-let { PathResolver } = require('./dist/main')
+let { PathResolver } = require('./dist/index')
 
 // let directoryMap = {
 //     src: {
@@ -53,48 +52,47 @@ let { PathResolver } = require('./dist/main')
 // debug(resolveSrc('index.js'))
 
 const directoryMap = {
-    src: {
-        _: 'source',
-        navigation: {
-            index: 'index.js',
-            menu: 'menu.js'
-        },
-        index: 'index.js'
-    },
-    dist: {
-        app: 'app.bundle.js'
-    },
-    scss: {
-        _: '@style',
-        manifest: '_manifest.scss',
-        app: 'app.scss',
-    }
+	src: {
+		_: 'source',
+		navigation: {
+			index: 'index.js',
+			menu: 'menu.js'
+		},
+		index: 'index.js'
+	},
+	dist: {
+		app: 'app.bundle.js'
+	},
+	scss: {
+		_: '@style',
+		manifest: '_manifest.scss',
+		app: 'app.scss',
+	}
 }
 
 const appPaths = new PathResolver(directoryMap)
 
 function isCyclic (obj) {
-    var seenObjects = [];
-  
-    function detect (obj) {
-      if (obj && typeof obj === 'object') {
-        if (seenObjects.indexOf(obj) !== -1) {
-          return true;
-        }
-        seenObjects.push(obj);
-        for (var key in obj) {
-          if (obj.hasOwnProperty(key) && detect(obj[key])) {
-            console.log(obj, 'cycle at ' + key);
-            return true;
-          }
-        }
-      }
-      return false;
-    }
-  
-    return detect(obj);
-  }
+	var seenObjects = [];
+
+	function detect (obj) {
+		if (obj && typeof obj === 'object') {
+			if (seenObjects.indexOf(obj) !== -1) {
+			return true;
+			}
+			seenObjects.push(obj);
+			for (var key in obj) {
+			if (obj.hasOwnProperty(key) && detect(obj[key])) {
+				console.log(obj, 'cycle at ' + key);
+				return true;
+			}
+			}
+		}
+		return false;
+	}
+
+	return detect(obj);
+}
 let { files, paths } = appPaths
 
 print(paths)
-print(files)
--- src/PathResolver.js
+++ src/PathResolver.js
@@ -9,225 +9,299 @@ import { isFunction, isObject, isString } from '@beautiful-code/type-utils'
 import { DuplicateKeyError, InvalidArgumentsError } from './errors'
 
 export class PathResolver {
-    static defaultConfig = {
-        rootPath: fs.realpathSync(process.cwd()),
-        paths: {},
-        options: {
-            namespace: 'paths',
-            fileroot: 'files',
-            rootPath: undefined,
-            resolverPrefix: 'resolve'        
-        }
-    }
+	static defaultConfig = {
+		rootPath: fs.realpathSync(process.cwd()),
+		paths: {},
+		options: {
+			namespace: 'paths',
+			fileroot: 'files',
+			rootPath: undefined,
+			resolverPrefix: 'resolve'
+		}
+	}
 
-    // Class Initialization
-    
-    constructor(...args){
-        let { rootPath, paths, options } = this._handleArgs(...args)
-        this.options = options
+	// Class Initialization
+	
+	constructor(...args){
+		let {rootPath, paths, options} = this._handleArgs(...args)
+		this.options = options
 
-        this.init(rootPath, paths)
-    }
+		this.init({rootPath, paths})
+	}
 
-    init = (rootPath, paths) => {
-        let { namespace, fileroot } = this.options  
-        let directoryResolver
-        if(namespace){
-            this[namespace] = {}
-            directoryResolver = this[namespace]
-        } else {
-            directoryResolver = this
-        }
-       
-        const resolveLevel = (localPaths, parentPath, scope, index = 0) => {
-            let localResolver = Object.entries(localPaths).reduce((resolver, [key, value]) => {
-                if(isString(value)) {
-                    if(this.isFileURI(value)){
-                        let fullpath = this._formatFullPath(value, parentPath, rootPath)
-                        resolver[key] = fullpath
-                    }
-                    
-                } else if(isObject(value)) {
-                    let localRootPath = parentPath ? `${parentPath}/${key}` : key
-                    let resolverKey, localScope
+	init = ({rootPath, paths} ) => {
+		let {namespace, fileroot} = this.options  
+		let directoryResolver
+		
+		if(namespace){
+			this[namespace] = {}
+			this[namespace]['aliases'] = {}
+			directoryResolver = this[namespace]
+		} else {
+			directoryResolver = this
+		}
 
-                    if(this.hasAlias(value)){
-                        localScope = this.getLocalScope(value, scope)
-                        resolverKey = this.getDirectoryResolverKey(localScope)
-                    } else if(this.hasLocalAlias(value)){
-                        localScope = this.getLocalScope(value, scope)
-                        resolverKey = this.getDirectoryResolverKey(localScope, scope)
-                    } else {
-                        localScope = scope
-                        resolverKey = this.getDirectoryResolverKey(key, localScope)
-                    }
-                    let resolverPath = path.resolve(rootPath, key)
-                    let resolverFunction = this._addPathToResolver(directoryResolver, resolverKey, resolverPath)
+		directoryResolver['aliases'] = {}
+		let aliasResolver = directoryResolver['aliases']
 
-                    let nextScope = this.getNextScope(value, key, scope)
-                    let nextParent = this.getNextParentPath(key, parentPath)
+		const resolveAliases = ({
+			localPaths,
+			parentPath, 
+			scope, 
+			index = 0}) => {
+			
 
-                    resolver[key] = resolveLevel(value, nextParent, nextScope, ++index)
-                    resolver[key]['_root'] = path.resolve(rootPath, localRootPath)
-                }
-                return resolver
-            }, {})
-            return localResolver
-        }
+			const localAliasResolver = Object.entries(localPaths).reduce((resolver, [key, value]) => {
+				if(this.hasAlias(value)){
+					let aliasScope = this.getLocalScope(value, scope)
+					let aliasResolverKey = this.getDirectoryResolverKey(aliasScope)
+					let resolverPath = path.resolve(rootPath, key)
+					let resolverFunction = this._addPathToResolver(directoryResolver, aliasResolverKey, resolverPath)
+				}
 
-        let files = resolveLevel(paths)
-        this[fileroot] = files
-        this[fileroot]['_root'] = rootPath
-    }
 
-    // Public Methods
+				let nextScope = this.getNextScope(key, scope)
+				let nextParent = this.getNextParentPath(key, parentPath)
 
-    getDirectoryResolverKey = (key, scope) => {
-        let result
-        if(scope && key){
-            let keyToResolve = `${scope}-${key}`
-            result = this._formatResolverKey(keyToResolve)
-        } else if(key){
-            result = this._formatResolverKey(key)
-        }
-        return result
-    }
+				resolveAliases({
+					localPaths: value, 
+					parentPath: nextParent, 
+					scope: nextScope, 
+					index: ++index})
+			})
+			
+		}
 
-    getLocalScope = (object, scope) => {
-        if(object.hasOwnProperty('_')){
-            return this.hasAlias(object) ? object._.slice(1) : object._
-        } else {
-            return scope
-        }
-    }
+		const resolveLevel = ({
+			localPaths,
+			parentPath, 
+			scope, 
+			index = 0}) => {
 
-    getNextParentPath = (key, parentPath) => parentPath ? `${parentPath}/${key}` : key
+			const localResolver = Object.entries(localPaths).reduce((resolver, [key, value]) => {
+				if(isString(value) && this.isFileURI(value)){
+					let fullpath = this._formatFullPath(value, parentPath, rootPath)
+					resolver[key] = fullpath
+				} else if(isObject(value)){
+					let localRootPath = parentPath ? `${parentPath}/${key}` : key
+					let localScope = scope
+					let resolverKey = this.getDirectoryResolverKey(key, localScope)
 
-    getNextScope = (object, key, scope) => {
-        if(this.hasAlias(object)){
-            return object._.slice(1)
-        } else if(scope && this.hasLocalAlias(object)){
-            return `${scope}-${object._}`
-        } else if(scope && !object._){
-            return `${scope}-${key}`
-        } else if(!scope && object._){
-            return object._
-        } else {
-            return key
-        }
-    }
+					let resolverPath = path.resolve(rootPath, key)
+					let resolverFunction = this._addPathToResolver(directoryResolver, resolverKey, resolverPath)
 
-    hasAlias = (object) => object.hasOwnProperty('_') && object._.indexOf('@') == 0    
-    hasLocalAlias = (object) => object.hasOwnProperty('_')
+					let nextScope = this.getNextScope(key, scope)
+					let nextParent = this.getNextParentPath(key, parentPath)
 
-    haveDuplicateKeys = (object, other) => {
-        let result = false     
-        Object.keys(object).map((key) => {
-            if(other.hasOwnProperty(key)){
-                result = true
-                return false
-            }
-        })
-        return result
-    }
+					resolver[key] = resolveLevel({
+						localPaths: value, 
+						parentPath: nextParent, 
+						scope: nextScope, 
+						index: ++index})
+					resolver[key]['_root'] = path.resolve(rootPath, localRootPath)
+				}
 
-    isFileURI = (uri) => {
-        return uri.split('/').pop().indexOf('.') > -1
-    }
+				return resolver
+			}, {})
 
-    isDirectoryURI = (uri) => {
-        return path.split('/').pop().indexOf('.') == -1
-    }
+			return localResolver
+		}
+	
+		const resolveLevelOld = (localPaths, parentPath, scope, index = 0) => {
+			let localResolver = Object.entries(localPaths).reduce((resolver, [key, value]) => {
+				if(isString(value)) {
+					if(this.isFileURI(value)){
+						let fullpath = this._formatFullPath(value, parentPath, rootPath)
+						resolver[key] = fullpath
+					}
+					
+				} else if(isObject(value)) {
+					let localRootPath = parentPath ? `${parentPath}/${key}` : key
+					let resolverKey, localScope
 
-    makeRelativeResolver = (rootPath) => (relativePath = '') => path.resolve(rootPath, relativePath)
-    resetScope = (parentPath) => toSnakeCase(parentPath.replace(/\\\//))
+					if(this.hasAlias(value)){
+						localScope = this.getLocalScope(value, scope)
+						resolverKey = this.getDirectoryResolverKey(localScope)
+					} else if(this.hasLocalAlias(value)){
+						localScope = this.getLocalScope(value, scope)
+						resolverKey = this.getDirectoryResolverKey(localScope, scope)
+					} else {
+						localScope = scope
+						resolverKey = this.getDirectoryResolverKey(key, localScope)
+					}
+					let resolverPath = path.resolve(rootPath, key)
+					let resolverFunction = this._addPathToResolver(directoryResolver, resolverKey, resolverPath)
 
-    // Private Methods
+					let nextScope = this.getNextScope(value, key, scope)
+					let nextParent = this.getNextParentPath(key, parentPath)
 
-    _addPathToResolver = (directoryResolver, resolverKey, resolverPath) => {
-        this._resolverHasPropertyEmpty(directoryResolver, resolverKey, () => {
-            directoryResolver[resolverKey] = this.makeRelativeResolver(resolverPath)
-        })
-    }
+					resolver[key] = resolveLevel(value, nextParent, nextScope, ++index)
+					resolver[key]['_root'] = path.resolve(rootPath, localRootPath)
+				}
+				return resolver
+			}, {})
+			return localResolver
+		}
 
-    _formatFullPath = (filename, parentPath, rootPath) => parentPath && rootPath 
-    ? path.resolve(rootPath, parentPath, filename) 
-    : parentPath || rootPath 
-        ? path.resolve(parentPath || rootPath , filename) 
-        : filename
+		let files = resolveLevel({localPaths: paths})
+		resolveAliases({localPaths: paths})
+		this[fileroot] = files
+		this[fileroot]['_root'] = rootPath
+	}
 
-    _formatResolverKey = (key) => toCamelCase(`${this.options.resolverPrefix}-${toSnakeCase(key)}`)
+	// Public Methods
 
-    _handleArgs = (...args) => {
-        let rootPath, paths, options
+	getDirectoryResolverKey = (key, scope) => {
+		let result
+		if(scope && key){
+			let keyToResolve = `${scope}-${key}`
+			result = this._formatResolverKey(keyToResolve)
+		} else if(key){
+			result = this._formatResolverKey(key)
+		}
+		return result
+	}
 
-        switch(args.length){
-            case 0: {
-                warn(`A 'PathResolver' object was passed no initial arguments. Initialization must be done manually.`)
-            } break
-            case 1: {
-                if(isObject(args[0])){
-                    paths = args[0]
-                    options = {}
-                }
-            } break
-            case 2: {
-                if(isObject(args[0]) && isObject(args[1])){
-                    paths = args[0]
-                    options = args[1]
-                }
-                if(isString(args[0]) && isObject(args[1])){
-                    rootPath = args[0]
-                    paths = args[1]
-                }
-            } break
-            case 3: {
-                rootPath = args[0]
-                paths = args[1]
-                options = args[2]
-            } break
-            default: {
-                throw new InvalidArgumentsError(`PathResolver accepts between 1-3 arguments, '${args.length}' found.`)
-            }              
-        }
+	getLocalScope = (object, scope) => {
+		if(object.hasOwnProperty('_')){
+			return this.hasAlias(object) ? object._.slice(1) : object._
+		} else {
+			return scope
+		}
+	}
 
-        if(!rootPath && options && options.rootPath){
-            rootPath = options.rootPath
-        }
-        
-        let config = { 
-            rootPath: rootPath || PathResolver.defaultConfig.rootPath,
-            paths: paths || PathResolver.defaultConfig.paths,
-            options: Object.assign({}, PathResolver.defaultConfig.options, options)
-        }
+	getNextParentPath = (key, parentPath) => parentPath ? `${parentPath}/${key}` : key
+	getNextScope = (key, scope) => scope ? `${scope}-${key}` : key
 
-        options = this._validateOptions(config.options)        
-        return config
-    }
+	getNextScopeOld = (object, key, scope) => {
+		if(this.hasAlias(object)){
+			return object._.slice(1)
+		} else if(scope && this.hasLocalAlias(object)){
+			return `${scope}-${object._}`
+		} else if(scope && !object._){
+			return `${scope}-${key}`
+		} else if(!scope && object._){
+			return object._
+		} else {
+			return key
+		}
+	}
 
-    _resolverHasPropertyEmpty(obj, key, onSuccess, onError){
-        let contains = obj.hasOwnProperty(key)
-        try {
-            if(contains){
-                throw new DuplicateKeyError(`The given object has duplicate keys '${key}'. Make sure all directories have unique key, or use scopes / aliases'`)
-            } else {
-                isFunction(onSuccess) ? onSuccess(false) : undefined
-            } 
-        } catch (e){
-            isFunction(onError) ? onError(true, e) : error(e)
-        }
-        return contains
-    }
+	hasAlias = (object) => object.hasOwnProperty('_') && object._.indexOf('@') == 0    
+	hasLocalAlias = (object) => object.hasOwnProperty('_')
 
-    _validateOptions(options){
-        let { namespace, fileroot } = options
-        namespace = isString(namespace) && namespace.trim().length > 0 ? namespace : undefined
-        fileroot = isString(fileroot) && fileroot.trim().length > 0 ? fileroot : undefined
-        
-        return {
-            namespace,
-            fileroot,
-            ...options
-        }        
-    }
+	haveDuplicateKeys = (object, other) => {
+		let result = false     
+		Object.keys(object).map((key) => {
+			if(other.hasOwnProperty(key)){
+				result = true
+				return false
+			}
+		})
+		return result
+	}
+
+	isFileURI = (uri) => {
+		return uri.split('/').pop().indexOf('.') > -1
+	}
+
+	isDirectoryURI = (uri) => {
+		return path.split('/').pop().indexOf('.') == -1
+	}
+
+	makeRelativeResolver = (rootPath) => (relativePath = '') => path.resolve(rootPath, relativePath)
+	resetScope = (parentPath) => toSnakeCase(parentPath.replace(/\\\//))
+
+	// Private Methods
+
+	_addPathToResolver = (resolver, resolverKey, resolverPath) => {
+		this._resolverHasPropertyEmpty(resolver, resolverKey, () => {
+			resolver[resolverKey] = this.makeRelativeResolver(resolverPath)
+		})
+	}
+
+	_addAliasToResolver = (key, relativeResolver) => {
+		return this._resolverHasPropertyEmpty(aliasResolver, key, () => {
+			return aliasResolver[key] = relativeResolver()
+		})
+	}
+
+	_formatFullPath = (filename, parentPath, rootPath) => 
+		parentPath && rootPath 
+			? path.resolve(rootPath, parentPath, filename) 
+			: parentPath || rootPath 
+				? path.resolve(parentPath || rootPath , filename) 
+				: filename
+
+	_formatResolverKey = (key) => toCamelCase(`${this.options.resolverPrefix}-${toSnakeCase(key)}`)
+
+	_handleArgs = (...args) => {
+		let rootPath, paths, options
+
+		switch(args.length){
+			case 0: {
+				warn(`A 'PathResolver' object was passed no initial arguments. Initialization must be done manually.`)
+			} break
+			case 1: {
+				if(isObject(args[0])){
+					([paths] = args)
+					options = {}
+				}
+			} break
+			case 2: {
+				if(isObject(args[0]) && isObject(args[1])){
+					([paths, options] = args[0])
+				}
+				if(isString(args[0]) && isObject(args[1])){
+					[rootPath, paths] = args
+				}
+			} break
+			case 3: {
+				[rootPath, paths, options] = args
+			} break
+			default: {
+				throw new InvalidArgumentsError(`PathResolver accepts between 1-3 arguments, '${args.length}' found.`)
+			}              
+		}
+
+		if(!rootPath && options && options.rootPath){
+			rootPath = options.rootPath
+		}
+		
+		let config = { 
+			rootPath: rootPath || PathResolver.defaultConfig.rootPath,
+			paths: paths || PathResolver.defaultConfig.paths,
+			options: Object.assign({}, PathResolver.defaultConfig.options, options)
+		}
+
+		options = this._validateOptions(config.options)        
+		return config
+	}
+
+	_resolverHasPropertyEmpty(obj, key, onSuccess, onError){
+		let contains = obj.hasOwnProperty(key)
+		try {
+			if(contains){
+				throw new DuplicateKeyError(`The given object has duplicate keys '${key}'. Make sure all directories have unique key, or use scopes / aliases'`)
+			} else {
+				return isFunction(onSuccess) ? onSuccess(false) : undefined
+			} 
+		} catch (e){
+			isFunction(onError) ? onError(true, e) : error(e)
+			return false
+		}
+		return contains
+	}
+
+	_validateOptions(options){
+		let { namespace, fileroot } = options
+		namespace = isString(namespace) && namespace.trim().length > 0 ? namespace : undefined
+		fileroot = isString(fileroot) && fileroot.trim().length > 0 ? fileroot : undefined
+		
+		return {
+			namespace,
+			fileroot,
+			...options
+		}        
+	}
 }
